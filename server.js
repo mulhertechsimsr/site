c4d6bfc809f5c1b1dbec6069a7121e0a94522d66
VanessaPessoa <vanessalima@cc.ci.ufpb.br>
2020-02-11 10:34:01 -0200

    add btn-close

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Umbler listening on 	port: ${port}`);
 });
