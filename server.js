e9df7375a1f238378ab6a5e2cb865fdef5405681
VanesssaPessoa <vanessalima@cc.ci.ufpb.br>
2020-02-12 10:40:41 -0300

    alteração img das redes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Umbler listening on 	port: ${port}`);
 });
