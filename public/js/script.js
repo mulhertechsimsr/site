3cd5ddfad4075fe0f81165655c1ff855b9c5a8ee
Marianna <mariannactx@gmail.com>
2020-02-26 10:40:40 -0300

    adicionar estrutura do contador

--- public/js/script.js
+++ public/js/script.js
@@ -29,9 +29,40 @@ const setupMenu = () => {
   });
 }
 
+const countDown = () => {
+  var countDate = new Date("2020-03-28 08:00:00").getTime();
+
+  // Update the count down every 1 second
+  var countInterval = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  byId("count-down-days").innerHTML = days;
+  byId("count-down-hours").innerHTML = hours;
+  byId("count-down-minutes").innerHTML = minutes;
+  byId("count-down-seconds").innerHTML = seconds;
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(countInterval);
+  }
+}, 1000);
+}
+
 window.addEventListener("load", () => {
   setupMenu();
   sizes();
+  countDown();
 });
 
 window.addEventListener("resize", sizes);
